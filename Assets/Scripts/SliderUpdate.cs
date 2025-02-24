dbad138b1df9409778c8b3d40bc037dd69f53de0
Love <lovelito02@gmail.com>
2025-02-24 12:05:46 +0100

    FEAT: Add seed logging, step by step delay, optimize tile generation

--- Assets/Scripts/SliderUpdate.cs
+++ Assets/Scripts/SliderUpdate.cs
@@ -9,37 +9,88 @@ using UnityEngine.UI;
 
 public class SliderUpdate : MonoBehaviour
 {
-	public GenerateButton GenButton;
+	public GenerateButton genButton;
 	public TMP_InputField inputField;
+	public TextMeshProUGUI sbsText;
+	public TileManager manager;
+	public bool genSlider;
     // Start is called before the first frame update
     void Start()
     {
-		// GenButton = GetComponentInParent<GenerateButton>();
-		// textBox = GetComponentInChildren<TextMeshProUGUI>();
+		if (genSlider)
+		{
+			genButton.mapSize = 10;
+			inputField.SetTextWithoutNotify(genButton.mapSize.ToString());
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (genSlider)
+		{
+			if (genButton.mapSize <= 1)
+			{
+				genButton.mapSize = 2;
+				inputField.SetTextWithoutNotify(genButton.mapSize.ToString());
+			}
+		}
+		// else if (!genSlider)
+		// {
+		// 	if (manager.sbsStepTime < 0.01)
+		// 	{
+		// 		manager.sbsStepTime = 0.01f;
+		// 		inputField.SetTextWithoutNotify(manager.sbsStepTime.ToString("F2"));
+		// 	}
+		// 	else if (manager.sbsStepTime > 1.0f)
+		// 	{
+		// 		manager.sbsStepTime = 1.0f;
+		// 		inputField.SetTextWithoutNotify(manager.sbsStepTime.ToString("F2"));
+		// 	}
+		// }
+	}
+
+	//
 	public void UpdateValue()
 	{
 		int val = GetComponentInChildren<Slider>().value.ConvertTo<int>();
- 		GenButton.MapSize = val;
+ 		genButton.mapSize = val;
 		inputField.SetTextWithoutNotify(val.ToString());
 	}
+	
+	//
 	public void GetTypedValue()
 	{
-		// string valString = GetComponentInChildren<InputField>().text;
 		string valString = inputField.text;
 		if (int.TryParse(valString, out int newNum))
 		{
-			GenButton.MapSize = newNum;
+			genButton.mapSize = newNum;
 			inputField.SetTextWithoutNotify(newNum.ToString());
 			GetComponent<Slider>().SetValueWithoutNotify(newNum);
 		}
 		else 
-			inputField.SetTextWithoutNotify(GenButton.MapSize.ToString());
+			inputField.SetTextWithoutNotify(genButton.mapSize.ToString());
 	}
+
+	//
+	public void UpdateSBSDelay()
+	{
+		manager.sbsStepTime = GetComponentInChildren<Slider>().value;
+		sbsText.SetText(manager.sbsStepTime.ToString("F2"));
+		// inputField.SetTextWithoutNotify(manager.sbsStepTime.ToString("F2"));
+	}
+
+	//
+	// public void GetTypedSBSDelayValue()
+	// {
+	// 	string valString = inputField.text;
+	// 	if (int.TryParse(valString, out int newNum))
+	// 	{
+	// 		manager.sbsStepTime = newNum;
+	// 		inputField.SetTextWithoutNotify(newNum.ToString());
+	// 		GetComponent<Slider>().SetValueWithoutNotify(newNum);
+	// 	}
+	// 	else 
+	// 		inputField.SetTextWithoutNotify(manager.sbsStepTime.ToString("F2"));
+	// }
 }
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,130 +1,176 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.VisualScripting;
-using Unity.Mathematics;
+using UnityEngine.UI;
+using System;
+using System.IO;
 using UnityEngine.Tilemaps;
 
 public class TileManager : MonoBehaviour
 {
-	//List of tile positions
-	public List<Vector2> TileMap = new List<Vector2>();
-	//Next tile's X position
-	float NextTilePosX = 0.0f;
-	//Next tile's Y position
-	float NextTilePosY = 0.0f;
-	Vector3 nextRotation = new Vector3(0,0,90);
-	// The size of the map used as: MapSize x MapSize
-	public int MapSize = 10;
-	//A list of the tiles prefabs
-	// public List<GameObject> TilesPrefabs;
-	//Tiles placed in the world
-	public List<WFCTile> PlacedTiles = new();
-	//The gameobject that should parent the tiles
-	public GameObject TilesParent;
+	//LIST OF TILE POSITIONS
+	// public List<Vector2> tilePosMap = new();
+	//MAP OF TILES AND THEIR LOCATIONS
+	public Dictionary<Vector2, WFCTile> tileMap = new();
+	//TILES PLACED IN THE WORLD
+	public List<WFCTile> placedTiles = new();
+	//THE GAMEOBJECT THAT SHOULD PARENT THE TILES
+	public GameObject tilesParent;
+	//THE PREFAB FOR TILES
 	public WFCTile tileObj;
-	bool fullySpawned = false;
-	public bool stepByStep = false;
-	int tilePosIndex = 0;
-	int tilesPerFrame = 1;
+	//SEED UI TOGGLE
+	public Toggle seedToggle;
+	//STEP BY STEP UI TOGGLE
+	public Toggle sbsToggle;
+	//WAVE FUNCTION COLLAPSE SCRIPT
+	WFC wfc;
 
-    // Start is called before the first frame update
+	// THE SIZE OF THE MAP USED AS: MAPSIZE X MAPSIZE
+	public int mapSize = 10;
+	//LARGEST MAP DIMENSION SO FAR
+	int largestMapSize = 0;
+
+	//SEED USED FOR THE RANDOM NUMBER GENERATOR 
+	int seed = 0;
+	//FILEPATH TO SEED LOG
+	string seedLogPath = "Assets/seeds.log";	
+
+	/*--------STEP BY STEP VARIABLES--------*/
+	/*USED IN A FOR LOOP FOR THE STEP BY STEP TO DIRECT	
+	HOW MANY TILES SHOULD BE PLACED PER LOOP*/
+	int tilesPerFrame = 1;
+	//USED DURING STEP BY STEP FOR KNOWING WHICH TILE IT'S AT
+	int tilePosIndex = 0;
+	//BOOL TO CHECK IF THE STEP BY STEP SHOULD CONTINUE
+	bool fullySpawned = false;
+	//BOOL TO CHECK IF STEP BY STEP SHOULD BE SETUP
+	bool stepByStep = false;
+	//TIME AT LAST STEP
+	float timerStart = 0.0f;
+	//TIME ELAPSED FROM LAST STEP
+	float timerElapsed = 0.0f;
+	//TIME BETWEEN EACH STEP IN SECONDS
+	public float sbsStepTime = 0.5f;
+	/*--------------------------------------*/
+
+
+    // START IS CALLED BEFORE THE FIRST FRAME UPDATE
     void Start()
     {
-		MapSize = 10;
-		GenerateMap();
+		mapSize = 10;
+		wfc = GetComponent<WFC>();
+		RegenerateMap();
     }
 
-    // Update is called once per frame
+    // UPDATE IS CALLED ONCE PER FRAME
     void Update() 
 	{
-		for (int i = 0; i < tilesPerFrame; i++)
-		 {
-			if (!fullySpawned && stepByStep)
+		timerElapsed = Time.time - timerStart;
+		if (!fullySpawned && stepByStep && timerElapsed >= sbsStepTime)
+		{
+			for (int i = 0; i < tilesPerFrame; i++)
 			{
 				StepGenerateMap();
 			}
-		 }
+			timerStart = Time.time;
+		}
 	}
 
+	//GENERATE A MAP WITHOUT STEP BY STEP
 	public void GenerateMap()
 	{
-		TileMap.Clear();
-        for (int i = 1; i <= MapSize * MapSize; i++)
+		foreach (WFCTile tile in placedTiles)
 		{
-			Vector2 TilePos = Vector2.zero;
-			TilePos.x = NextTilePosX;
-			TilePos.y = NextTilePosY;
-			NextTilePosX += 1;
-			// Increase Y
-			if (i % MapSize == 0 && i != 0)
-			{
-				NextTilePosX = 0.0f;
-				NextTilePosY += 1;
-			}
-			TileMap.Add(TilePos);
-		}
-		//Spawn all tiles
-		for (int i = 0; i < TileMap.Count; i++)
-		{
-			Quaternion quat = new(0, 0, 0, 1);
-			nextRotation.z = UnityEngine.Random.Range(0, 3) * 90;
-			quat.eulerAngles = nextRotation;
-			// GameObject tile = Instantiate(TilesPrefabs[UnityEngine.Random.Range(0,6)], TilesParent);
-			WFCTile tile = Instantiate(tileObj, new Vector3(TileMap[i].x, TileMap[i].y, 0), Quaternion.identity);
-			tile.transform.SetParent(TilesParent.transform);
-			tile.SetManager(this);
-			PlacedTiles.Add(tile);
+			if (tile.GetTileLoc().x < mapSize && tile.GetTileLoc().y < mapSize)
+				tile.SelectSprite();
 		}
 		fullySpawned = true;
-		
 	}
+
+	//SPAWN NEW TILES
+	void SpawnTiles()
+	{
+        for (int i = 0; i < mapSize * mapSize; i++)
+		{
+			if (!tileMap.ContainsKey(new(i % mapSize, i / mapSize)))
+			{
+				WFCTile tile = Instantiate(tileObj, new Vector3(i % mapSize, i / mapSize, 0), Quaternion.identity);
+				tileMap.Add(new(i % mapSize, i / mapSize), tile);
+				tile.SetTileLoc(new(i % mapSize, i / mapSize));
+				tile.transform.SetParent(tilesParent.transform);
+				tile.SetManager(GetComponent<TileManager>());
+				placedTiles.Add(tile);
+			}
+		}
+	}
+	public WFCTile GetTileFromLoc(Vector2 loc)
+	{
+		if (tileMap.ContainsKey(loc))
+			return tileMap[loc];
+		else
+			return null;
+	}
+	//RESTART GENERATION OF MAP AND DECIDE WHAT TYPE OF GENERATION
 	public void RegenerateMap()
 	{
-		foreach (WFCTile WFCtile in PlacedTiles)
+		if (mapSize > largestMapSize)
 		{
-			Destroy(WFCtile.gameObject);
+			largestMapSize = mapSize;
+			SpawnTiles();
 		}
-		PlacedTiles.Clear();
-		TileMap.Clear();
-		tilePosIndex = 1;
-		NextTilePosX = 0.0f;
-		NextTilePosY = 0.0f;
+		foreach (WFCTile tile in placedTiles)
+		{
+			tile.ResetSprite();
+		}
+
+		if (seedToggle.isOn)
+		{
+			seed = UnityEngine.Random.Range(0, 10000);
+		}
+		UnityEngine.Random.InitState(seed);
+		LogSeed();
+
+		stepByStep = sbsToggle.isOn;
 		if (stepByStep)
 		{
+			tilePosIndex = 0;
 			fullySpawned = false;
+			StepGenerateMap();
 			return;
 		}
 		GenerateMap();
 	}
+
+	//GENERATE A MAP WITH STEP BY STEP
 	public void StepGenerateMap()
 	{
-        if (tilePosIndex <= MapSize*MapSize)
-		{
-			Vector2 TilePos = Vector2.zero;
-			TilePos.x = NextTilePosX;
-			TilePos.y = NextTilePosY;
-			NextTilePosX += 1;
-			// Increase Y
-			if (tilePosIndex % MapSize == 0 && tilePosIndex != 0)
-			{
-				print(tilePosIndex);
-				NextTilePosX = 0.0f;
-				NextTilePosY += 1;
-			}
-			TileMap.Add(TilePos);
-		}
+		placedTiles[tilePosIndex].SelectSprite();
 
 		//Spawn next tile
-		WFCTile tile = Instantiate(tileObj, new Vector3 (TileMap[tilePosIndex-1].x, TileMap[tilePosIndex-1].y, 0), Quaternion.identity);
-		tile.transform.SetParent(TilesParent.transform);
-		tile.SetManager(this);
-		PlacedTiles.Add(tile);
-		tilePosIndex ++;
+		// WFCTile tile = Instantiate(tileObj, new Vector3 (TilePosMap[tilePosIndex-1].x, TilePosMap[tilePosIndex-1].y, 0), Quaternion.identity);
+		// tile.transform.SetParent(TilesParent.transform);
+		// tile.SetManager(this);
+		// PlacedTiles.Add(tile);
 
-		if (PlacedTiles.Count == MapSize * MapSize)
+		tilePosIndex ++;
+		if (tilePosIndex == mapSize * mapSize)
 		{
 			fullySpawned = true;
+		}
+	}
+
+	public void LogSeed()
+	{
+		string formattedSeed = seed.ToString("D4");
+		DateTime now = DateTime.Now;
+		string timeStamp = now.ToString("yyyy-MM-dd HH:mm:ss");
+		string logEntry = $"{timeStamp}: {formattedSeed}";
+		try
+		{
+			File.AppendAllText(seedLogPath, logEntry + "\n\n");
+		}
+		catch(System.Exception ex)
+		{
+			UnityEngine.Debug.LogError("Error logging seed: " + ex.Message);
 		}
 	}
 }
