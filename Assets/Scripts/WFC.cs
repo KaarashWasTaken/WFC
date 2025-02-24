1b89fe643c99db1bac6c2377b8d056147381ab5b
Love <lovelito02@gmail.com>
2025-02-24 12:06:09 +0100

    FEAT: Add wfc script

---
+++ Assets/Scripts/WFC.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WFC : MonoBehaviour
+{
+    //TILEMANAGER COMPONENT
+    TileManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GetComponent<TileManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
