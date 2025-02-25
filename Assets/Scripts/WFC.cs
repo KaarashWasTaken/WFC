6f14fc7ea965eca86e17fd956af2e7b2b76e9c73
Love <lovelito02@gmail.com>
2025-02-25 15:07:11 +0100

    FEAT: Add new logic to WFCTile, not complete yet

--- Assets/Scripts/WFC.cs
+++ Assets/Scripts/WFC.cs
@@ -6,6 +6,7 @@ public class WFC : MonoBehaviour
 {
     //TILEMANAGER COMPONENT
     TileManager manager;
+	Queue<WFC2Tile> tileQueue;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,154 @@ public class WFC : MonoBehaviour
     void Update()
     {
         
+    }
+
+	public void Add(WFC2Tile tile)
+	{
+		tileQueue.Enqueue(tile);
+	}
+	// [System.Serializable]
+    // public class Tile
+    // {
+    //     public GameObject prefab;
+    //     public List<string> compatibleNeighborsUp;
+    //     public List<string> compatibleNeighborsDown;
+    //     public List<string> compatibleNeighborsLeft;
+    //     public List<string> compatibleNeighborsRight;
+    // }
+
+    // public Tile[] tileTypes;
+    public int width = 10;
+    public int height = 10;
+    public Vector3 startPosition = Vector3.zero;
+    public float tileSpacing = 1f;
+
+    private Tile?[][] grid; // Nullable Tile, represents possible tile states
+
+    void Start()
+    {
+        InitializeGrid();
+        Collapse();
+    }
+
+    void InitializeGrid()
+    {
+        grid = new Tile?[width][];
+        for (int x = 0; x < width; x++)
+        {
+            grid[x] = new Tile?[height];
+            for (int y = 0; y < height; y++)
+            {
+                grid[x][y] = null; // Initially, all cells are uncollapsed and have all possibilities.
+            }
+        }
+    }
+
+    void Collapse()
+    {
+        List<(int x, int y)> uncollapsedCells = new List<(int x, int y)>();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                uncollapsedCells.Add((x, y));
+            }
+        }
+
+        while (uncollapsedCells.Count > 0)
+        {
+            (int x, int y) = FindLowestEntropy(uncollapsedCells);
+            CollapseCell(x, y);
+            UpdateNeighbors(x, y);
+            uncollapsedCells.Remove((x, y));
+        }
+
+        InstantiateGrid();
+    }
+
+    (int x, int y) FindLowestEntropy(List<(int x, int y)> uncollapsedCells)
+    {
+        (int x, int y) lowestEntropyCell = uncollapsedCells[0];
+        int lowestEntropy = int.MaxValue;
+
+        foreach ((int x, int y) cell in uncollapsedCells)
+        {
+            int entropy = CalculateEntropy(cell.x, cell.y);
+            if (entropy < lowestEntropy)
+            {
+                lowestEntropy = entropy;
+                lowestEntropyCell = cell;
+            }
+        }
+        return lowestEntropyCell;
+    }
+
+    int CalculateEntropy(int x, int y)
+    {
+        if (grid[x][y] != null) return int.MaxValue; // Already collapsed.
+
+        List<Tile> possibleTiles = GetPossibleTiles(x, y);
+        return possibleTiles.Count;
+    }
+
+    void CollapseCell(int x, int y)
+    {
+        List<Tile> possibleTiles = GetPossibleTiles(x, y);
+        if (possibleTiles.Count == 0)
+        {
+            Debug.LogError("No possible tiles at (" + x + ", " + y + "). Wave function collapse failed.");
+            return;
+        }
+
+        Tile chosenTile = possibleTiles[Random.Range(0, possibleTiles.Count)];
+        grid[x][y] = chosenTile;
+    }
+
+    List<Tile> GetPossibleTiles(int x, int y)
+    {
+        if (grid[x][y] != null) return new List<Tile> { grid[x][y].Value }; // Already collapsed.
+
+        List<Tile> possibleTiles = tileTypes.ToList();
+
+        // Check Neighbors
+        if (y > 0 && grid[x][y - 1] != null) // Down
+        {
+            possibleTiles = possibleTiles.Where(tile => grid[x][y - 1].Value.compatibleNeighborsUp.Contains(tile.prefab.name)).ToList();
+        }
+        if (y < height - 1 && grid[x][y + 1] != null) // Up
+        {
+            possibleTiles = possibleTiles.Where(tile => grid[x][y + 1].Value.compatibleNeighborsDown.Contains(tile.prefab.name)).ToList();
+        }
+        if (x > 0 && grid[x - 1][y] != null) // Left
+        {
+            possibleTiles = possibleTiles.Where(tile => grid[x - 1][y].Value.compatibleNeighborsRight.Contains(tile.prefab.name)).ToList();
+        }
+        if (x < width - 1 && grid[x + 1][y] != null) // Right
+        {
+            possibleTiles = possibleTiles.Where(tile => grid[x + 1][y].Value.compatibleNeighborsLeft.Contains(tile.prefab.name)).ToList();
+        }
+
+        return possibleTiles;
+    }
+
+    void UpdateNeighbors(int x, int y)
+    {
+        // Add logic to propagate constraints if needed. For simple cases, this is not required.
+    }
+
+    void InstantiateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Tile? tile = grid[x][y];
+                if (tile != null)
+                {
+                    Vector3 position = startPosition + new Vector3(x * tileSpacing, 0, y * tileSpacing);
+                    Instantiate(tile.Value.prefab, position, Quaternion.identity);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/WFCTile.cs
+++ Assets/Scripts/WFCTile.cs
@@ -2,16 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Animations;
 
 public enum TileType
 {
-    None,
-    Grass,
+	Corner,
     Dirt,
-    Path
+	Flowers,
+    Grass,
+	Inner,
+	Straight,
+    None,
+	LASTINLIST
 }
 public class WFCTile : MonoBehaviour
 {
@@ -20,25 +25,25 @@ public class WFCTile : MonoBehaviour
     //ALL SPRITES
     public List<SpriteRenderer> spriteRenderers;
     //SPRITES WITH TILETYPE ALLOWED BASED ON NEIGHBOURS
-    Dictionary<SpriteRenderer, TileType> allowedSprites = new();
-    //DIRECTIONS WHICH THE TILE IS OPEN WHEN IT IS A PATH TYPE
-    // public Dictionary<string, bool> openDirections = new()
-    // {
-    //     {"North", false},
-    //     {"East", false},
-    //     {"South", false},
-    //     {"West", false}
-    // };
+    Dictionary<TileType, Quaternion> allowedSprites = new();
     //DIRECTIONS WHICH THE TILE IS OPEN WHEN IT IS A PATH TYPE: 0:NORTH 1:WEST 2:SOUTH 3:EAST
     public List<bool> openDirections = new(){false, false, false, false};
     //0:NORTH 1:WEST 2:SOUTH 3:EAST
-    public List<WFCTile> adjacentTiles = new();
+    public List<WFC2Tile> adjacentTiles = new();
     //THE TILEMANAGER THAT CREATED THIS TILE
     public TileManager manager;
     //THE ACTIVE AND VISIBLE SPRITE
     SpriteRenderer activeSprite;
     //THE LOCATION OF THE TILE
     Vector2 tileLoc;
+
+	/*--------ALLOWED NEIGHBOURS--------*/
+	List<Dictionary<TileType, Quaternion>> allowedUpNeighbour = new();
+	List<Dictionary<TileType, Quaternion>> allowedLeftNeighbour = new();
+	List<Dictionary<TileType, Quaternion>> allowedDownNeighbour = new();
+	List<Dictionary<TileType, Quaternion>> allowedRightNeighbour = new();
+	/*----------------------------------*/
+
 
     // START IS CALLED BEFORE THE FIRST FRAME UPDATE
     void Awake()
@@ -75,12 +80,12 @@ public class WFCTile : MonoBehaviour
             {
                 spriteRenderers[i].enabled = true;
                 activeSprite = spriteRenderers[i];
-                tileType = allowedSprites[activeSprite];
+                tileType = allowedSprites[];
                 transform.Rotate(Vector3.forward, UnityEngine.Random.Range(0,4) * 90);
-                if (tileType == TileType.Path)
-                {
-                    AssignOpen();
-                }
+                // if (tileType == TileType.Path)
+                // {
+                //     AssignOpen();
+                // }
             }
             else
             {
@@ -97,59 +102,66 @@ public class WFCTile : MonoBehaviour
     }
 
     //ASSIGN THE OPEN DIRECTIONS TO THE OPENDIRECTIONS LIST
-    void AssignOpen()
-    {
-        //ALL DIRECTIONS ARE OPEN AT THE INNER CORNER
-        if (activeSprite == spriteRenderers[4])
-        {
-            for (int i = 0; i < openDirections.Count; i++)
-            {
-                openDirections[i] = true;
-            }
-        }
-        else
-        {
-            //ROTATE WITH NEGATIVE SINCE MY LIST IS ORDERED OPPOSITE OF HOW THE UNIT CIRCLE ROTATES
-            if (activeSprite == spriteRenderers[5])
-            {
-                openDirections[(0 + (int)(transform.rotation.eulerAngles.z / 90) % 4 + 4) % 4] = true;
-                openDirections[(2 + (int)(transform.rotation.eulerAngles.z / 90) % 4 + 4) % 4] = true;
-                openDirections[(3 + (int)(transform.rotation.eulerAngles.z / 90) % 4 + 4) % 4] = true;
-            }
-            else
-            {
-                openDirections[(0 + (int)(transform.rotation.eulerAngles.z / 90) % 4 + 4) % 4] = true;
-                openDirections[(3 + (int)(transform.rotation.eulerAngles.z / 90) % 4 + 4) % 4] = true;
-            }
-        }
-    }
+    void AssignOpenDirections()
+	{
+		for (int i = 0; i < openDirections.Count; i++)
+		{
+			openDirections[i] = false;
+		}
+
+		if (tileType == TileType.Inner)
+		{
+			for (int i = 0; i < openDirections.Count; i++)
+			{
+				openDirections[i] = true;
+			}
+		}
+		else if (tileType == TileType.Straight)
+		{
+			openDirections[0] = true;
+			openDirections[2] = true;
+		}
+		else if (tileType == TileType.Corner)
+		{
+			openDirections[0] = true;
+			openDirections[3] = true;
+		}
+		//Rotate directions to match rotation.
+		int rotation = (int)(transform.rotation.eulerAngles.z / 90);
+		List<bool> rotatedDirections = new List<bool>(openDirections);
+		for (int i = 0; i < 4; i++)
+		{
+			openDirections[i] = rotatedDirections[(i - rotation + 4) % 4];
+		}
+	}
 
     //HIDE ALL SPRITES
     public void ResetSprite()
     {
-        foreach (SpriteRenderer sprite in spriteRenderers)
+		for (int i = 0; i < (int)TileType.LASTINLIST; i++)
         {
-            if (allowedSprites.ContainsKey(sprite))
+            if (allowedSprites.ContainsKey((TileType)i))
             {
-                allowedSprites.Remove(sprite);
+                allowedSprites.Remove((TileType)i);
             }
         }
         for (int i = 0; i < spriteRenderers.Count; i++)
         {
-            if (i == 0 || i == 4 || i == 5)
-            {
-                allowedSprites.Add(spriteRenderers[i], TileType.Path);
-            }
-            else if (i == 1)
-            {
-                allowedSprites.Add(spriteRenderers[i], TileType.Dirt);
-            }
-            else if (i == 2 || i == 3)
-            {
-                allowedSprites.Add(spriteRenderers[i], TileType.Grass);
-            }
+			allowedSprites.Add((TileType)i, Quaternion.identity);
+            // if (i == 0 || i == 4 || i == 5)
+            // {
+            //     allowedSprites.Add(spriteRenderers[i], TileType.Path);
+            // }
+            // else if (i == 1)
+            // {
+            //     allowedSprites.Add(spriteRenderers[i], TileType.Dirt);
+            // }
+            // else if (i == 2 || i == 3)
+            // {
+            //     allowedSprites.Add(spriteRenderers[i], TileType.Grass);
+            // }
             spriteRenderers[i].enabled = false;
-            if (spriteRenderers[i].name == "Empty")
+            if (spriteRenderers[(int)TileType.None])
             {
                 spriteRenderers[i].enabled = true;
             }
@@ -164,30 +176,14 @@ public class WFCTile : MonoBehaviour
     //FIND ALL TILE NEIGHBOURS
     void FindAdjacent()
     {
-        if (manager == null)
-        {
-            transform.parent.GetComponentInParent<TileManager>();
-        }
-        for (int i = 1; i <= 4; i++)
-        {
-            float xDiff = Mathf.RoundToInt(Mathf.Cos(math.radians(90 * i)));
-            float yDiff = Mathf.RoundToInt(Mathf.Sin(math.radians(90 * i)));
-            try
-            {
-            adjacentTiles.Add(manager.GetTileFromLoc(new(tileLoc.x + xDiff, tileLoc.y + yDiff)));
-                print("Added: " + adjacentTiles[i-1]);
-            }
-            catch(System.NullReferenceException)
-            {
-                print("Added null to adjacents");
-                print(manager);
-                adjacentTiles.Add(null);
-            }
-            catch(System.Exception ex)
-            {
-                Debug.LogError("An unexpected error occurred: " + ex.Message);
-            }
-        }
+		//NORTH
+		adjacentTiles[0] = manager.GetTileFromLoc(tileLoc + Vector2.up);
+		//WEST
+        adjacentTiles[1] = manager.GetTileFromLoc(tileLoc + Vector2.left);
+		//SOUTH
+        adjacentTiles[2] = manager.GetTileFromLoc(tileLoc + Vector2.down);
+		//EAST
+        adjacentTiles[3] = manager.GetTileFromLoc(tileLoc + Vector2.right);
     }
 
     //FIND ALLOWED SPRITES
@@ -200,5 +196,190 @@ public class WFCTile : MonoBehaviour
     void Update()
     {
 
+    }
+}
+public class WFC2Tile : MonoBehaviour
+{
+	//THIS TILE'S TYPE
+    public TileType tileType;
+	//LIST OF ALL SPRITES
+    public List<SpriteRenderer> spriteRenderers;
+	//LIST OF THIS TILE'S POSSIBLE TYPES
+    public List<TileType> possibleTileTypes = new();
+	//THIS TILE'S OPEN DIRECTIONS
+    public List<bool> openDirections = new() { false, false, false, false };
+	//
+    public WFC2Tile[] adjacentTiles = new WFC2Tile[4];
+    public TileManager manager;
+    public Vector2 tileLoc;
+
+    public int rotation = 0;
+	//ALLOWED NEIGHBOURS BASED ON TYPE AND ROTATION
+    public Dictionary<TileType, List<int>>[] allowedNeighbors = new Dictionary<TileType, List<int>>[4];
+
+    void Awake()
+    {
+        ResetTile();
+    }
+
+    public void SetManager(TileManager newManager)
+    {
+        manager = newManager;
+    }
+
+    public void SetTileLoc(Vector2 loc)
+    {
+        tileLoc = loc;
+    }
+
+    public Vector2 GetTileLoc()
+    {
+        return tileLoc;
+    }
+
+    public void CollapseTile()
+    {
+        if (possibleTileTypes.Count == 0)
+        {
+            Debug.LogError("No possible tiles at " + tileLoc);
+            return;
+        }
+
+        tileType = possibleTileTypes[Random.Range(0, possibleTileTypes.Count)];
+        rotation = GetPossibleRotations(tileType)[Random.Range(0, GetPossibleRotations(tileType).Count)];
+        UpdateSprite();
+        AssignOpenDirections();
+        PropagateConstraints();
+    }
+
+    void UpdateSprite()
+    {
+        for (int i = 0; i < spriteRenderers.Count; i++)
+        {
+            spriteRenderers[i].enabled = (i == (int)tileType);
+        }
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+    }
+
+    List<int> GetPossibleRotations(TileType type)
+    {
+        List<int> possibleRotations = new List<int>() { 0, 90, 180, 270 };
+        for (int i = 0; i < 4; i++)
+        {
+            if (adjacentTiles[i] != null && adjacentTiles[i].tileType != TileType.None)
+            {
+                if (adjacentTiles[i].allowedNeighbors[(i + 2) % 4].ContainsKey(type))
+                {
+                    List<int> allowed = adjacentTiles[i].allowedNeighbors[(i + 2) % 4][type];
+                    possibleRotations = possibleRotations.Intersect(allowed).ToList();
+                }
+                else
+                {
+                    possibleRotations.Clear();
+                    break;
+                }
+            }
+        }
+        return possibleRotations;
+    }
+
+    void PropagateConstraints()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (adjacentTiles[i] != null)
+            {
+                adjacentTiles[i].UpdatePossibleTileTypes(this, (i + 2) % 4);
+            }
+        }
+    }
+
+    public void UpdatePossibleTileTypes(WFC2Tile neighbor, int direction)
+    {
+        List<TileType> newPossibleTypes = new List<TileType>(possibleTileTypes);
+        foreach (TileType type in possibleTileTypes)
+        {
+            if (!allowedNeighbors[direction].ContainsKey(type))
+            {
+                newPossibleTypes.Remove(type);
+            }
+            else
+            {
+                List<int> possibleRotations = GetPossibleRotations(type);
+                List<int> allowedRotations = allowedNeighbors[direction][type];
+                if (possibleRotations.Intersect(allowedRotations).ToList().Count == 0)
+                {
+                    newPossibleTypes.Remove(type);
+                }
+            }
+        }
+        possibleTileTypes = newPossibleTypes;
+    }
+
+    public void ResetTile()
+    {
+        possibleTileTypes.Clear();
+        for (int i = 0; i < (int)TileType.LASTINLIST; i++)
+        {
+            possibleTileTypes.Add((TileType)i);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            allowedNeighbors[i] = new Dictionary<TileType, List<int>>();
+            for (int j = 0; j < (int)TileType.LASTINLIST; j++)
+            {
+                allowedNeighbors[i].Add((TileType)j, new List<int>() { 0, 90, 180, 270 });
+            }
+        }
+
+        for (int i = 0; i < spriteRenderers.Count; i++)
+        {
+            spriteRenderers[i].enabled = i == (int)TileType.None;
+        }
+        transform.rotation = Quaternion.identity;
+    }
+
+    void FindAdjacent()
+    {
+		//NORTH
+		adjacentTiles[0] = manager.GetTileFromLoc(tileLoc + Vector2.up);
+		//WEST
+        adjacentTiles[1] = manager.GetTileFromLoc(tileLoc + Vector2.left);
+		//SOUTH
+        adjacentTiles[2] = manager.GetTileFromLoc(tileLoc + Vector2.down);
+		//EAST
+        adjacentTiles[3] = manager.GetTileFromLoc(tileLoc + Vector2.right);
+    }
+
+    void AssignOpenDirections()
+    {
+        for (int i = 0; i < openDirections.Count; i++)
+        {
+            openDirections[i] = false;
+        }
+
+        if (tileType == TileType.Inner)
+        {
+            for (int i = 0; i < openDirections.Count; i++)
+            {
+                openDirections[i] = true;
+            }
+        }
+        else if (tileType == TileType.Straight)
+        {
+            openDirections[0] = true;
+            openDirections[2] = true;
+        }
+        else if (tileType == TileType.Corner)
+        {
+            openDirections[0] = true;
+            openDirections[3] = true;
+        }
+        int rotationIndex = (int)(transform.rotation.eulerAngles.z / 90);
+        List<bool> rotatedDirections = new List<bool>(openDirections);
+        for (int i = 0; i < 4; i++)
+        {
+            openDirections[i] = rotatedDirections[(i - rotationIndex + 4) % 4];
+        }
     }
 }
